f05dfd5157a91ca2c262b669b864ef179e9b6d7b
tbc-beren <beren.themoonlightshade@gmail.com>
2021-01-30 20:17:35 +0100

    Added missing license header

--- test/testHttpRequest.cpp
+++ test/testHttpRequest.cpp
@@ -1,3 +1,17 @@
+/**
+*
+* The Black Codex Library: Chapter: BCSockets
+*
+* https://github.com/tbc-beren/BCSockets
+*
+* Mozilla Public License Version 2.0
+* https://github.com/tbc-beren/BCSockets/blob/master/LICENSE
+*
+* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+* If a copy of the MPL was not distributed with this file, You can obtain one at
+* https://mozilla.org/MPL/2.0/.
+*
+*/
 #include <BCSockets/http/BCHttpExchange.hpp>
 
 #include <gtest/gtest.h>
